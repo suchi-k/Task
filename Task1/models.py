6a85a4d802153b0093a5943fba2ba05796f6e1f2
K Sucharitha <ksucharitha131097@gmail.com>
2024-04-26 14:18:55 +0530

    applied new migrations files

--- Task1/models.py
+++ Task1/models.py
@@ -1,16 +1,18 @@
 from django.db import models
-from django.import Book,Author,Rental
 
 
 # Create your models here.
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.ForeignKey(Author,on_delete =models.CASCADE)
-    publication_year = models.IntegerField(max_length=10)
+
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
     biography = models.TextField()
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author,on_delete =models.CASCADE)
+    publication_year = models.IntegerField()
+
 
 class Rental(models.Model):
     book_Id = models.ForeignKey(Book,on_delete=models.CASCADE) 

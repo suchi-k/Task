29b76eeac329b7d4e2451783463a9e55c38aba28
K Sucharitha <ksucharitha131097@gmail.com>
2024-04-26 19:11:48 +0530

    dummy paractice file

---
+++ Task1/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from .views import *
+urlspatterns = [
+    path('',Task1_list),
+]
--- Task1/views.py
+++ Task1/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def Task1_list(request):
+    return view

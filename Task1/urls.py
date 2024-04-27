a45c419085ef9bb85ffcbe4645db0ebd88317f6a
K Sucharitha <ksucharitha131097@gmail.com>
2024-04-27 12:27:57 +0530

    added files

--- Task1/urls.py
+++ Task1/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import *
-urlspatterns = [
-    path('',Task1_list),
+from .views import book_list
+
+
+urlpatterns = [
+     path('book/', book_list, name='book_list'),
 ]
--- Task1/views.py
+++ Task1/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-
+#from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
-def Task1_list(request):
-    return view
+@api_view(['GET','PUT'])
+def book_list(request):
+    if request.method == 'POST':
+        return Response({"message":"Got some data!","data":request})
+    return Response({"message":"Hello World!"})
